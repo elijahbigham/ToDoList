8ec96b3de06707d334bcf6becc202082053280eb
GreenHam09 <elijahbigham@gmail.com>
2020-02-21 10:50:40 -0500

    Did step two

--- ToDoList/toDoList.swift
+++ ToDoList/toDoList.swift
@@ -22,10 +22,35 @@ class toDoList: UITableViewController {
     indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier:
         "ToDoCellIdentifier", for: indexPath)
-     
+        
         let todo = toDos[indexPath.row]
         cell.textLabel?.text = todo.title
         return cell
+    }
+    override func tableView(_ tableView: UITableView,
+    numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let savedToDos = toDoList.loadToDos(){
+            toDos = savedToDos
+        } else {
+            toDos = toDoList.loadSampleToDos()
+        }
+    }
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    static func loadSampleToDos() -> [ToDo]{
+        let todo1 = ToDo(title: "ToDo One", isComplete: false, dueDate: Date(), notes: "Notes 1")
+        
+        let todo2 = ToDo(title: "ToDo Two", isComplete: false, dueDate: Date(), notes: "Notes 2")
+        
+        let todo3 = ToDo(title: "ToDo Three", isComplete: false, dueDate: Date(), notes: "Notes 3")
+        
+            return [todo1, todo2, todo3]
     }
 
     
